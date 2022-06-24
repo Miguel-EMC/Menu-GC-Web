7a9d93c5d8897c0d1624e98b607d27678c2c56b0
Miguel-EMC <eduardomuzo123456@gmail.com>
2022-06-24 01:43:44 -0500

    Menu-GC/Web

---
+++ resources/views/home.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styles.css" rel="stylesheet" />
+    <title>Menu</title>
+</head>
+
+<body>
+    <!-- ENCABEZADO DEL MENU -->
+    <header>
+        <div class="wrp">
+            <div class="portada">
+                <h1>CG/Web</h1>
+                <h2>Menu Principal</h2>
+            </div>
+            <div class="curveado">
+                <img src="img/img2.svg">
+            </div>
+        </div>
+    </header>
+
+    <!-- CUADROS DEL CUERPO DEL MENU -->
+    <main>
+        <div class="grid">
+            <div class="card">
+                <img src="img/EPMMOP.png" alt="EPMMOP">
+                <div class="footer-card">
+                    <a href="http://192.168.0.120:8001/CGWEB_E2/CGWebInicio/Inicio.aspx" target="_blank">EPMMOP</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="img/METRO.jpg" alt="METRO">
+                <div class="footer-card">
+                    <a href="http://192.168.0.120:8100/CGWEB_E1/CGWebInicio/Inicio.aspx" target="_blank">METRO</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <img src="img/LDUQuito.png" alt="LDUQ">
+                <div class="footer-card">
+                    <a href="http://192.168.0.120:8080/CGWEB_E1/CGWebInicio/Inicio.aspx" target="_blank">LDU</a>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- PIE DE PAGINA -->
+    <footer>
+        <div class="footer-content">
+            <h2>:: ENTORNO ::</h2>
+            <h2>CG VERSION 3.0</h2>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; CG/Web V.3.0 by Aggity | 2021</p>
+        </div>
+    </footer>
+</body>
+</html>
